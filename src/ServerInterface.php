00eecb6d44bba93e096f19517435088b8eb17f23
wheakerd <wheakerd@qq.com>
2024-01-29 16:28:57 +0800

    update

--- src/ServerInterface.php
+++ src/ServerInterface.php
@@ -9,9 +9,29 @@ namespace Skernl\Contract;
  */
 interface ServerInterface
 {
+    /**
+     * @see SWOOLE_SOCK_TCP
+     */
     public const int SERVER_HTTP = SWOOLE_SOCK_TCP;
+
+    /**
+     * @see SWOOLE_SOCK_TCP
+     */
     public const int SERVER_WEBSOCKET = SWOOLE_SOCK_TCP;
+
+    /**
+     * @see SWOOLE_SOCK_UDP
+     */
     public const int SERVER_UDP = SWOOLE_SOCK_UDP;
+
+    /**
+     * @see SWOOLE_UNIX_STREAM
+     */
     public const int SERVER_UNIX_STREAM = SWOOLE_UNIX_STREAM;
+
+    /**
+     * @see SWOOLE_SOCK_TCP
+     * @see SWOOLE_SSL
+     */
     public const int SERVER_HTTPS = SWOOLE_SOCK_TCP | SWOOLE_SSL;
 }
